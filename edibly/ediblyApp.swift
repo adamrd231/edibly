148ed63a7b95b65803063778a15977c82abe176b
adamrd231 <adam.rdconcepts@gmail.com>
2021-06-30 13:49:08 -0600

    Initial Commit

---
+++ edibly/ediblyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ediblyApp.swift
+//  edibly
+//
+//  Created by Adam Reed on 6/30/21.
+//
+
+import SwiftUI
+
+@main
+struct ediblyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
